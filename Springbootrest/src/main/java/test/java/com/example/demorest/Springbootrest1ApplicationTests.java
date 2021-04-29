866b668846ff4138f727cda18a17ea3674589f31
ksvikas424 <srinivasvikask@gmail.com>
2021-04-29 11:38:17 +0530

    SpringBootRest

---
+++ Springbootrest/src/main/java/test/java/com/example/demorest/Springbootrest1ApplicationTests.java
@@ -0,0 +1,14 @@
+package test.java.com.example.demorest;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootrest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
